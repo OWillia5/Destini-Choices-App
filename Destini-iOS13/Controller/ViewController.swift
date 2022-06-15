7320b5c07bf2bf65dd89128af135424adce5a4d4
owillia5 <davwilliams6@gmail.com>
2022-06-14 19:56:03 -0500

    Edited App Brewery's base "Destini" app. Updated the UI with cooler images and proper layouts. Applied MVC design pattern to structure the logic and view for the app. Added StoryBrain for the logic

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -4,17 +4,48 @@
 //
 //  Created by Angela Yu on 08/08/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
-//
+//  Edited by Opeoluwa Williams
 
 import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var storyBrain = StoryBrain()
+    
+    //moved stories array and story number to storybrain
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+       updateUI()
+        
     }
-
-
+    
+    //update the story and choices based on what choice user selects
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let userChoice = sender.currentTitle!
+        
+        storyBrain.nextStory(userChoice)
+        
+        updateUI()
+        
+        
+    }
+    
+    func updateUI(){
+        //update the UI (display story and choice labels)
+        storyLabel.text = storyBrain.getStoryText()
+        
+        choice1Button.setTitle(storyBrain.getChoiceOne(), for: .normal)
+        
+        choice2Button.setTitle(storyBrain.getChoiceTwo(), for: .normal)
+    }
+        
 }
+
+
 
--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -4,6 +4,26 @@
 //
 //  Created by Angela Yu on 08/08/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
-//
+// Edited by Opeoluwa Williams
 
 import Foundation
+
+struct Story{
+    let title: String
+    
+    let choice1: String
+    
+    let choice2: String
+    
+    let choice1Destination: Int
+    
+    let choice2Destination: Int
+    
+    init(t: String, c1: String, c1D: Int, c2: String, c2D: Int){
+        title = t
+        choice1 = c1
+        choice1Destination = c1D
+        choice2 = c2
+        choice2Destination = c2D
+    }
+}
